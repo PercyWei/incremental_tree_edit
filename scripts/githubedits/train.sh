efff2aaaeac311d4368f990e9e71a3d0be3df978
percyw <1353005228@qq.com>
2023-08-20 19:02:49 +0800

    测试 git 推送流程

--- scripts/githubedits/train.sh
+++ scripts/githubedits/train.sh
@@ -3,9 +3,10 @@
 source activate structural_edits
 export CUDA_VISIBLE_DEVICES=0
 
-export PYTHONPATH=/scratch/yao.470/CMU_project/incremental_tree_edit:$PYTHONPATH
+# export PYTHONPATH=/scratch/yao.470/CMU_project/incremental_tree_edit:$PYTHONPATH
+export PYTHONPATH=/root/projects/Graph2Edit:$PYTHONPATH
 
-seed=0    # remember to change this!
+seed=7  # remember to change this!
 config_file=$1
 branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 commit=$(git rev-parse HEAD | cut -c 1-7)
@@ -19,11 +20,11 @@ mkdir -p ${work_dir}
 
 # TODO 1: uncomment the training setting
 # TODO 2: consider adding `--small_memory` to disable training data preprocessing
-#OMP_NUM_THREADS=1 python -u -m exp_githubedits train \
-# 	--cuda \
-#	--seed=${seed} \
-#	--work_dir=${work_dir} \
-#    ${config_file} 2>${work_dir}/err.log
+OMP_NUM_THREADS=1 python -u -m exp_githubedits train \
+ 	--cuda \
+	--seed=${seed} \
+	--work_dir=${work_dir} \
+    ${config_file} 2>${work_dir}/err.log
 
 #OMP_NUM_THREADS=1 python -u -m exp_githubedits imitation_learning \
 #	--cuda \
